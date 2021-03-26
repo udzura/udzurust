87ea8c5a7f194a5173fa4f6543ccbf1f4441e3eb
Uchio Kondo <udzura@udzura.jp>
2021-03-26 19:41:23 +0900

    Make proper code of Rust FFI

--- remruby/src/main.rs
+++ remruby/src/main.rs
@@ -1,26 +1,25 @@
+use libc::c_char;
 use std::env::args;
 use std::ffi::CString;
-use libc::c_char;
 
-#[repr(C)]
-struct mrb_state_s;
-type mrb_state = *const mrb_state_s;
+enum MrbState {}
 
-#[link(name="mruby", kind="static")]
-extern {
-    fn mrb_open() -> mrb_state;
-    fn mrb_load_string(mrb: mrb_state, code: *const c_char);
-    fn mrb_close(mrb: mrb_state);
+#[link(name = "mruby", kind = "static")]
+extern "C" {
+    fn mrb_open() -> *mut MrbState;
+    fn mrb_load_string(mrb: *mut MrbState, code: *const c_char);
+    fn mrb_close(mrb: *mut MrbState);
 }
 
 fn main() {
     let code = args().nth(2);
     if let Some(c) = code {
         let bytes = c.into_bytes();
+        let code = CString::new(bytes).unwrap();
 
         unsafe {
-            let mut mrb = mrb_open();
-            mrb_load_string(mrb, CString::new(bytes).unwrap().as_ptr());
+            let mrb = mrb_open();
+            mrb_load_string(mrb, code.as_ptr());
             mrb_close(mrb);
         }
     } else {
